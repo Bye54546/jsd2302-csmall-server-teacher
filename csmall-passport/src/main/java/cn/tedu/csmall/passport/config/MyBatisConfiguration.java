888e6f927b11763495766a94f760dce73f193781
成恒 <83123095@qq.com>
2023-05-18 16:19:48 +0800

    .

---
+++ csmall-passport/src/main/java/cn/tedu/csmall/passport/config/MyBatisConfiguration.java
@@ -0,0 +1,9 @@
+package cn.tedu.csmall.passport.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("cn.tedu.csmall.passport.mapper")
+public class MyBatisConfiguration {
+}
