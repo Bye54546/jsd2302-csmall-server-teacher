86ca23b32a243d9ecba6ff3ac5b82a15f38b4134
成恒 <83123095@qq.com>
2023-05-18 09:44:48 +0800

    .

--- csmall-product/src/test/java/cn/tedu/csmall/product/mapper/AlbumMapperTests.java
+++ csmall-product/src/test/java/cn/tedu/csmall/product/mapper/AlbumMapperTests.java
@@ -30,6 +30,26 @@ public class AlbumMapperTests {
     }
 
     @Test
+    void updateById() {
+        Album album = new Album();
+        album.setId(1L);
+        album.setName("Redmi Note 11 5G的相册");
+        // album.setDescription("这家伙很懒，啥也没写");
+        // album.setSort(199);
+        // album.setGmtModified(LocalDateTime.now());
+
+        int rows = mapper.updateById(album);
+        System.out.println("修改数据完成，受影响的行数：" + rows);
+    }
+
+    @Test
+    void getStandardById() {
+        Long id = 1L;
+        Object queryResult = mapper.getStandardById(id);
+        System.out.println("根据【ID=" + id + "】查询数据完成，结果：" + queryResult);
+    }
+
+    @Test
     void list() {
         List<?> list = mapper.list();
         System.out.println("查询列表完成，结果集中的数据量：" + list.size());
