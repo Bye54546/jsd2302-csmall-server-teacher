a45436a864b14d2f6f29523d587438f12bfc98a7
成恒 <83123095@qq.com>
2023-05-24 16:01:39 +0800

    .

--- csmall-passport/src/main/java/cn/tedu/csmall/passport/filter/JwtAuthorizationFilter.java
+++ csmall-passport/src/main/java/cn/tedu/csmall/passport/filter/JwtAuthorizationFilter.java
@@ -1,7 +1,10 @@
 package cn.tedu.csmall.passport.filter;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
@@ -22,6 +25,24 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         // 根据业内惯用的做法，客户端会将JWT放在请求头（Request Header）中的Authorization属性中
         String jwt = request.getHeader("Authorization");
         log.debug("客户端携带的JWT：{}", jwt);
+
+        // 判断客户端是否携带了有效的JWT
+        if (!StringUtils.hasText(jwt)) {
+            // 如果JWT无效，则放行
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // TODO 当前类和AdminServiceImpl中都声明了同样的secretKey变量，是不合理的
+        // TODO 解析JWT过程中可能出现异常，需要处理
+        // 尝试解析JWT
+        String secretKey = "jhdSfkkjKJ3831HdsDkdfSA9jklJD749Fhsa34fdsKf08dfjFhkdfs";
+        Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody();
+
+        Long id = claims.get("id", Long.class);
+        String username = claims.get("username", String.class);
+        System.out.println("id = " + id);
+        System.out.println("username = " + username);
 
         // 放行
         filterChain.doFilter(request, response);
